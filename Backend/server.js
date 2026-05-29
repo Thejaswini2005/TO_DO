b2004a2d572e8cb33a2740447166ba10ac397c77
Thejaswini2005 <thejaswinigangavaram@gmail.com>
2026-05-29 11:08:19 +0530

    Render deployment fixes

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const app=require('./src/app');
 const connectDB=require('./src/DB/db');
 require('dotenv').config();
 connectDB();
+const PORT = process.env.PORT || 3000;
 app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
 });
