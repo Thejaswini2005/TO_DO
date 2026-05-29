b795c30c6fdf4171877975755fa650f7decb1e6d
Thejaswini2005 <thejaswinigangavaram@gmail.com>
2026-05-29 11:26:13 +0530

    Added home route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,5 +5,7 @@ const app = express();
 app.use(express.json() );
 app.use(cors());
 app.use('/tasks', taskRoutes);
- 
+ app.get('/', (req, res) => {
+    res.send('TaskFlow Backend API is running 🚀');
+});
 module.exports = app;
